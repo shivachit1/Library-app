cf550592ffcb1a12f1dcb4ebb592bc3394061ffd
shivachit1 <shivat@metropolia.fi>
2021-01-05 12:30:50 +0200

    uibuild

--- server.js
+++ server.js
@@ -205,6 +205,6 @@ server.applyMiddleware({
   app,
 });
 
-app.listen({ port: process.env.PORT || 4000 }, () => {
+server.listen({ port: process.env.PORT || 4000 }, () => {
   console.log(`🚀  Server ready at http://localhost:4000`);
 });
